65036f1001c274814ed46a627d0245f182a4fa2a
vehxi <vehxi.dev@gmail.com>
2026-04-30 06:47:17 +0300

    add handle

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,32 @@ function App() {
         content: '',
     })
 
+    const handePost = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        console.log(e.target.name);
+
+        setPost({...post, [name]: value });
+
+        console.log(post);
+    }
+
     return (
         <div className="bg-gray-50 min-h-screen p-4">
             <div className="mb-4 w-1/2 mx-auto bg-white p-4 border border-gray-200">
                 <div className="mb-4">
                     <input
-                        onChange={}
+                        onChange={(e) => handePost(e)}
+                        name="title"
                         placeholder="title"
                         type="text"
                         className="border border-gray-200 p-4 w-full"/>
                 </div>
                 <div className="mb-4">
                     <textarea
+                        onChange={(e) => handePost(e)}
                         placeholder="content"
+                        name="content"
                         className="border border-gray-200 p-4 w-full"/>
                 </div>
                 <div>
