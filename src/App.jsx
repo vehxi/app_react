1e1d04964f6a44080a128142957de7bd6ddbc610
vehxi <vehxi@MacBook-Air.local>
2026-04-30 06:20:56 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+
+
+    return (
+        <div className="bg-gray-50 min-h-screen p-4">
+            <div className="mb-4 w-1/2 mx-auto bg-white p-4 border border-gray-200">
+                <div className="mb-4">
+                    <input
+                        placeholder="title"
+                        type="text"
+                        className="border border-gray-200 p-4 w-full"/>
+                </div>
+                <div className="mb-4">
+                    <textarea
+                        placeholder="content"
+                        className="border border-gray-200 p-4 w-full"/>
+                </div>
+                <div>
+                    <a
+                        className="inline-block text-xs px-3 py-2 text-white bg-sky-600 border-sky-700"
+                        href="#">STORE</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
